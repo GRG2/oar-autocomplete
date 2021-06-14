23e202f343d4bc3132e7acf8737e2f2650cfcd9f
nhobson99 <nhobs1999@gmail.com>
2021-06-14 12:29:02 -0400

    removed unused code

--- autocomplete.py
+++ autocomplete.py
@@ -3,20 +3,21 @@ import pandas
 import json
 from difflib import SequenceMatcher
 import os
+
 try:
     import regex as re
 except:
     import re
 
-results = pandas.read_csv('parmenides_results.csv')
+results = pandas.read_csv("parmenides_results.csv")
 print("Results:", len(results))
 splits = []
 num_splits = max(os.cpu_count(), mp.cpu_count())
 len_results = len(results)
 
 for i in range(num_splits):
-    start = int(i*len_results/num_splits)
-    end = int((i+1)*len_results/num_splits)
+    start = int(i * len_results / num_splits)
+    end = int((i + 1) * len_results / num_splits)
     splits.append(results[start:end])
 
 THRESHOLD = 0.3
@@ -29,8 +30,9 @@ THRESHOLD = 0.3
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
+
 def match_terms(a, b, threshold):
-    if (len(a) == 0 or len(b) == 0):
+    if len(a) == 0 or len(b) == 0:
         return 0
     matches = 0
     words_a = set(a.split())
@@ -41,74 +43,60 @@ def match_terms(a, b, threshold):
             matches += similarity
     return matches / max(len(words_a), len(words_b))
 
+
 def search_helper(phrase, max_values, threshold, dataset, q):
     similarity_matrix = {}
     for index, row in dataset.iterrows():
         comparison_terms = row["TERM_REDUCED"].lower().strip()
-        comparison_terms = re.sub(r':\d:', ' ', comparison_terms)
+        comparison_terms = re.sub(r":\d:", " ", comparison_terms)
         comparison_phrase = row["PHRASE"].lower().strip()
-        similarity = max(match_terms(phrase.lower(), comparison_terms, threshold), similar(phrase, comparison_phrase))
-        if (comparison_phrase in similarity_matrix.keys()):
-            similarity_matrix[comparison_phrase] = max(similarity_matrix[comparison_phrase], similarity)
+        similarity = max(
+            match_terms(phrase.lower(), comparison_terms, threshold),
+            similar(phrase, comparison_phrase),
+        )
+        if comparison_phrase in similarity_matrix.keys():
+            similarity_matrix[comparison_phrase] = max(
+                similarity_matrix[comparison_phrase], similarity
+            )
         else:
             similarity_matrix[comparison_phrase] = similarity
-    
-    sorted_similarity_matrix = sorted(similarity_matrix.items(), key=lambda item: item[1], reverse=True)
-        
+
+    sorted_similarity_matrix = sorted(
+        similarity_matrix.items(), key=lambda item: item[1], reverse=True
+    )
+
     return_values = sorted_similarity_matrix[:max_values]
 
-    return_values = filter(lambda value : value[1] >= threshold, return_values)
+    return_values = filter(lambda value: value[1] >= threshold, return_values)
     q.put(list(return_values))
 
-# def search_helper(phrase, max_values, threshold, dataset, q):
-#     similarity_matrix = {}
-#     for index, row in dataset.iterrows():
-#         comparison_phrase = row["PHRASE"].lower().strip()
-#         similarity = similar(phrase.lower(), comparison_phrase)
-        
-#         similarity_matrix[comparison_phrase] = similarity
-    
-#     sorted_similarity_matrix = sorted(similarity_matrix.items(), key=lambda item: item[1], reverse=True)
-        
-#     return_values = sorted_similarity_matrix[:max_values]
-
-#     return_values = filter(lambda value : value[1] >= threshold, return_values)
-#     q.put(list(return_values))
-
-def search(phrase, max_values=10, threshold=THRESHOLD):
-    rv = ""
-    for line in search_helper(phrase, max_values, threshold):
-        if (rv != ""):
-            rv += "\n"
-        if (len(line[0]) >= 67):
-            rv += line[0][:63]
-            rv += "..."
-        else:
-            rv += line[0]
-    return rv
 
 def search_json(phrase, max_values=10, threshold=THRESHOLD):
     rv = []
     processes = []
     q = mp.Queue()
     for dataset in splits:
-        processes.append(mp.Process(target=search_helper, args=(phrase, max_values, threshold, dataset, q)))
+        processes.append(
+            mp.Process(
+                target=search_helper, args=(phrase, max_values, threshold, dataset, q)
+            )
+        )
     for p in processes:
         p.start()
-    
+
     return_values = []
 
     for p in processes:
         p.join()
         return_values += q.get()
-    
+
     return_values = sorted(return_values, key=lambda item: item[1], reverse=True)
 
     return_values_unique = []
     seen_values = set()
 
     for rv in return_values:
-        if (not rv[0] in seen_values):
+        if not rv[0] in seen_values:
             return_values_unique.append(rv)
             seen_values.add(rv[0])
 
