1a95bac31a3730fe3b54f84a03aba7c97c576fc2
nhobson99 <nhobs1999@gmail.com>
2021-06-14 12:29:32 -0400

    moved all code into main to alleviate multiprocessing on windows

--- server.py
+++ server.py
@@ -6,29 +6,27 @@ import signal
 import sys
 import time
 
-PORT = 8000
-
-
-class AutocompleteServer(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/json")
-        self.send_header("Access-Control-Allow-Origin", "*")
-        self.end_headers()
-        self.wfile.write(
-            bytes(autocomplete.search_json(parse.unquote(self.path[1:])), "utf-8")
-        )
-
-
-# https://stackoverflow.com/questions/1112343/how-do-i-capture-sigint-in-python
-def signal_handler(sig, frame):
-    global server
-    print("Received ctrl+c, exiting...")
-    server.server_close()
-    time.sleep(3)
-    sys.exit(0)
-
 if __name__ == "__main__":
+    PORT = 8000
+
+    class AutocompleteServer(http.server.BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            self.send_header("Content-type", "text/json")
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.end_headers()
+            self.wfile.write(
+                bytes(autocomplete.search_json(parse.unquote(self.path[1:])), "utf-8")
+            )
+
+    # https://stackoverflow.com/questions/1112343/how-do-i-capture-sigint-in-python
+    def signal_handler(sig, frame):
+        global server
+        print("Received ctrl+c, exiting...")
+        server.server_close()
+        time.sleep(3)
+        sys.exit(0)
+
     signal.signal(signal.SIGINT, signal_handler)
     server = None
     while server == None:
