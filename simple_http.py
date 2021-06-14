537f726eaef1d7950fdb74d476936b457c7eab0b
nhobson99 <nhobs1999@gmail.com>
2021-06-14 18:55:32 -0400

    added spinwait to http server

--- simple_http.py
+++ simple_http.py
@@ -5,6 +5,10 @@ PORT = 8080
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+while True:
+    try:
+        with socketserver.TCPServer(("", PORT), Handler) as httpd:
+            print("serving at port", PORT)
+            httpd.serve_forever()
+    except KeyboardInterrupt:
+        break
