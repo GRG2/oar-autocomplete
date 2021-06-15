42223815bdc90e8c7557d7ae54f419a273d2d776
nhobson99 <nhobs1999@gmail.com>
2021-06-15 14:54:44 -0400

    moved extension into new file

---
+++ ExpandedWriter.py
@@ -0,0 +1,76 @@
+
+from parmenides.conf import settings
+from parmenides.write import Writer
+
+import csv
+try:
+    import regex as re
+except:
+    import re
+
+chars_to_remove = ['.', ',', ';', '!', '?', '[', ']', '{', '}']
+chars_to_replace_with_space = ['_', '/', r'\s+', ':']
+
+
+MAX_TERMS = 5
+
+class ExpandedWriter(Writer):
+
+    def __init__(self, output):
+
+        fieldnames = [
+            'TERM',
+            'TERM_REDUCED',
+            'PHRASE',
+            'DOCUMENT',
+        ] + settings.CSV_EXTRA_COLUMNS
+
+        self.outfile = open(output, 'w', encoding=settings.ENCODING,
+                newline='')
+        self.writer = csv.DictWriter(self.outfile, delimiter=',',
+                quotechar='"', quoting=csv.QUOTE_MINIMAL,
+                fieldnames=fieldnames)
+
+        if settings.INCLUDE_HEADER:
+            self.writer.writeheader()
+
+    def remove_unwanted(self, s):
+        for c in chars_to_remove:
+            s = re.sub(re.escape(c), '', s, ignore_unused=True)
+        for c in chars_to_replace_with_space:
+            s = re.sub(c, ' ', s, ignore_unused=True)
+        return s
+
+    def write(self, document, trees):
+
+        collected_terms = {}
+        
+        for tree in trees:
+            term = tree.term
+            if term.representation in collected_terms:
+                collected_terms[term.representation].phrases |= \
+                    term.phrases
+            else:
+                term.sentence = str(tree)
+                collected_terms[term.representation] = term
+
+        for representation, term in collected_terms.items():
+            words = str(list(term.phrases)[0]).split()
+            if len(words) > MAX_TERMS:
+                continue
+            skip = False
+            for word in words:
+                if word.isnumeric() or not word.isalnum:
+                    skip = True
+                    break
+            if skip:
+                continue
+            row = {
+                'TERM': representation,
+                'TERM_REDUCED': " ".join(list(set(re.split(r':\d:', str(representation))))),
+                'PHRASE': self.remove_unwanted(str(list(term.phrases)[0])),
+                'DOCUMENT': document.identifier,
+            }
+            for column_name in settings.CSV_EXTRA_COLUMNS:
+                row[column_name] = document.metadata.get(column_name)
+            self.writer.writerow(row)
