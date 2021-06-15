bd40ddee042e5956674f26b47c9bf8da8144e485
nhobson99 <nhobs1999@gmail.com>
2021-06-14 23:00:51 -0400

    added documentation and cleaned up code

--- autocomplete.py
+++ autocomplete.py
@@ -1,47 +1,65 @@
-import multiprocessing as mp
 import json
 from difflib import SequenceMatcher
-
+# In case the better regex library is not available, use the standard library regex
 try:
     import regex as re
 except:
     import re
 
 
-
-# https://stackoverflow.com/a/17388505 "Find the similarity metric between two strings"
-# I figured this would be worth a shot instead of using cosine similarity
-# Plus it uses a builtin Python library, which is good for cutting down on dependencies
-# OPTIONAL: replace ratio() with quick_ratio() or real_quick_ratio() for a speed boost,
-#   at the cost of accuracy (leads to worse auto suggestions)
+'''
+https://stackoverflow.com/a/17388505 "Find the similarity metric between two strings"
+OPTIONAL: replace ratio() with quick_ratio() or real_quick_ratio() for a speed boost,
+at the cost of accuracy (leads to worse auto suggestions).
+returns: floating point number between 0 and 1
+'''
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 
+'''
+Finds the number of matching terms (fractional number, meaning partial matches
+give a value less than 1).
+Matches of less than threshold are ignored from the calculation entirely.
+This prevents matches over 100% (values greater than 1).
+returns: floating point number between 0 and 1
+'''
 def match_terms(a, b, threshold):
-    if len(a) == 0 or len(b) == 0:
-        return 0
     matches = 0
-    words_a = set(a.split())
+    words_a = set(a.split())  # Using sets to remove duplicate words
     words_b = set(b.split())
     for word_a in words_a:
+        best = 0
         for word_b in words_b:
             similarity = similar(word_a, word_b)
-            matches += similarity
+            best = max(best, similarity)
+        if (best > threshold):
+            matches += best
     return matches / max(len(words_a), len(words_b))
 
-
+'''
+Used in the process pool to analyze segments of the CSV data (see splits[] in server.py).
+arguments is a tuple, which is unpacked on the first line. The reason for using a tuple is
+that arguments are not able to be passed to a processing pool. This is a workaround.
+returns: a 2D list of strings and floating point numbers between 0 and 1
+'''
 def search_helper(arguments):
     dataset, phrase, max_values, threshold = arguments
+
+    # Similarity matrix keeps track of the best matches for the search phrase
     similarity_matrix = {}
+
     for index, row in dataset.iterrows():
+        # First, clean up both the terms and the phrase
         comparison_terms = row["TERM_REDUCED"].lower().strip()
-        comparison_terms = re.sub(r":\d:", " ", comparison_terms)
         comparison_phrase = row["PHRASE"].lower().strip()
+        # Then, find the highest similarity between the search phrase and the comparison data
+        # (match_terms for analyzing terms like keywords, and similar for matching whole phrases)
         similarity = max(
             match_terms(phrase.lower(), comparison_terms, threshold),
             similar(phrase, comparison_phrase),
         )
+        # Safeguard in case the phrase has already been seen before
         if comparison_phrase in similarity_matrix.keys():
             similarity_matrix[comparison_phrase] = max(
                 similarity_matrix[comparison_phrase], similarity
@@ -49,20 +67,27 @@ def search_helper(arguments):
         else:
             similarity_matrix[comparison_phrase] = similarity
 
-    sorted_similarity_matrix = sorted(
+    # Sort the similarity matrix from highest to lowest
+    similarity_matrix = sorted(
         similarity_matrix.items(), key=lambda item: item[1], reverse=True
     )
 
-    return_values = sorted_similarity_matrix[:max_values]
+    # Filter out matches below the threshold, then return only the number of values we want
+    return_values = filter(lambda value: value[1] >= threshold, similarity_matrix)
+    return_values = return_values[:max_values]
 
-    return_values = filter(lambda value: value[1] >= threshold, return_values)
-    return(list(return_values))
+    return list(return_values)
 
 
 def search_json(phrase, splits, p, max_values=10, threshold=0.5):
+    if (len(phrase) == 0):
+        return json.dumps([])
+
     rv = []
 
-    return_values = p.map(search_helper, [(split, phrase, max_values, threshold) for split in splits])
+    return_values = p.map(
+        search_helper, [(split, phrase, max_values, threshold) for split in splits]
+    )
     return_values = [item for sublist in return_values for item in sublist]
 
     return_values = sorted(return_values, key=lambda item: item[1], reverse=True)
--- simple_http.py
+++ simple_http.py
@@ -1,3 +1,16 @@
+'''
+Noah Hobson, nhobs1999@gmail.com
+simple_http.py
+
+All this program does is run a simple http server. It listens for requests and grants them.
+It uses the current directory as its target for serving web pages.
+This should NOT be used in final production because it is:
+a) Unsafe (only makes the most basic checks internally, and is not encrypted)
+b) Poorly made (and would not respond well to heavy load)
+c) Naive
+However, for a demo, it will be perfectly fine.
+'''
+
 import http.server
 import socketserver
 
