cad387b8a0a04c9fd6a2b9b855141005d3b2db79
nhobson99 <nhobs1999@gmail.com>
2021-06-17 16:12:50 -0400

    include %match and freq in json reply

--- autocomplete.py
+++ autocomplete.py
@@ -1,5 +1,6 @@
 import json
 from difflib import SequenceMatcher
+
 # In case the better regex library is not available, use the standard library regex
 try:
     import regex as re
@@ -7,24 +8,30 @@ except:
     import re
 
 
-'''
+"""
 https://stackoverflow.com/a/17388505 "Find the similarity metric between two strings"
 OPTIONAL: replace ratio() with quick_ratio() or real_quick_ratio() for a speed boost,
 at the cost of accuracy (leads to worse auto suggestions).
 returns: floating point number between 0 and 1
-'''
+"""
+
+
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 
-'''
+"""
 Finds the number of matching terms (fractional number, meaning partial matches
 give a value less than 1).
 Matches of less than threshold are ignored from the calculation entirely.
 This prevents matches over 100% (values greater than 1).
 returns: floating point number between 0 and 1
-'''
+"""
+
+
 def match_terms(a, b, threshold):
+    if similar(a.split()[0], b.split()[0]) < 0.9:
+        return 0
     matches = 0
     words_a = set(a.split())  # Using sets to remove duplicate words
     words_b = set(b.split())
@@ -33,16 +40,19 @@ def match_terms(a, b, threshold):
         for word_b in words_b:
             similarity = similar(word_a, word_b)
             best = max(best, similarity)
-        if (best > threshold):
+        if best > threshold:
             matches += best
     return matches / max(len(words_a), len(words_b))
 
-'''
+
+"""
 Used in the process pool to analyze segments of the CSV data (see splits[] in server.py).
 arguments is a tuple, which is unpacked on the first line. The reason for using a tuple is
 that arguments are not able to be passed to a processing pool. This is a workaround.
 returns: a 2D list of strings and floating point numbers between 0 and 1
-'''
+"""
+
+
 def search_helper(arguments):
     dataset, phrase, max_values, threshold = arguments
 
@@ -61,29 +71,41 @@ def search_helper(arguments):
         )
         # Safeguard in case the phrase has already been seen before
         if comparison_phrase in similarity_matrix.keys():
-            similarity_matrix[comparison_phrase] = max(
-                similarity_matrix[comparison_phrase], similarity
-            )
+            # similarity_matrix[comparison_phrase] = max(
+            #     similarity_matrix[comparison_phrase], similarity
+            # )
+            matrix_entry = similarity_matrix[comparison_phrase]
+            if similarity > matrix_entry["similarity"]:
+                similarity_matrix[comparison_phrase] = {
+                    "similarity": similarity,
+                    "data": dict(row),
+                }
         else:
-            similarity_matrix[comparison_phrase] = similarity
+            similarity_matrix[comparison_phrase] = {
+                "similarity": similarity,
+                "data": dict(row),
+            }
 
     # Sort the similarity matrix from highest to lowest
     similarity_matrix = sorted(
-        similarity_matrix.items(), key=lambda item: item[1], reverse=True
+        similarity_matrix.items(), key=lambda item: item[1]["similarity"], reverse=True
     )
 
     # Filter out matches below the threshold, then return only the number of values we want
-    return_values = list(filter(lambda value: value[1] >= threshold, similarity_matrix))
+    return_values = list(filter(lambda value: value[1]["similarity"] >= threshold, similarity_matrix))
     return_values = return_values[:max_values]
 
     return list(return_values)
 
-'''
+
+"""
 Boilerplate code which handles splitting the search between processes in the processing pool.
 returns: JSON formatted 2D list of strings and floating point numbers between 0 and 1
-'''
+"""
+
+
 def search_json(phrase, splits, p, max_values=10, threshold=0.5):
-    if (len(phrase) == 0):
+    if len(phrase) == 0:
         return json.dumps([])
 
     rv = []
@@ -93,7 +115,7 @@ def search_json(phrase, splits, p, max_values=10, threshold=0.5):
     )
     return_values = [item for sublist in return_values for item in sublist]
 
-    return_values = sorted(return_values, key=lambda item: item[1], reverse=True)
+    return_values = sorted(return_values, key=lambda item: item[1]["similarity"], reverse=True)
 
     return_values_unique = []
     seen_values = set()
@@ -104,4 +126,6 @@ def search_json(phrase, splits, p, max_values=10, threshold=0.5):
             seen_values.add(rv[0])
 
     return_values = return_values_unique[:max_values]
+    # return_values = sorted(return_values, key=lambda item: item[1]["data"]["FREQ"], reverse=True)
+    
     return json.dumps(return_values)
